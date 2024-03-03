8f9ee8fbe3aa7dedc3475836bf4501d33c35751d
obaid <obaidlone@gildwaretechnologies.com>
2024-03-03 14:52:08 +0530

    #setting structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import express,{Request,Response} from "express"
 import cors from "cors"
+import mongoose, { mongo } from "mongoose"
 import "dotenv/config"
+
+
+//DB connection
+mongoose.connect(process.env.DB_URL as string)
+
 
 const app =express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
+
+
+
 
 app.get("/api",async(req:Request,res:Response)=>{
 res.json({message:"Hello from api"})
---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+
+const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6">
+        <div className="container mx-auto flex justify-between">
+<span className="text-3xl text-white font-bold tracking-tight">
+<Link to="/">Alpin Travel</Link>
+</span>
+<span className="flex space-x-2">
+<Link to="/sign-in" className="bg-white flex items-center text-blue-600 px-3 font-bold hover:bg-yellow-100">Sign-In</Link>
+</span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
